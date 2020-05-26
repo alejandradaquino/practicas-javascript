ac84235750d53a44bc846e613f786e33e85fdade
alejandra <alejandra.juarez@etermax.com>
2020-05-25 21:18:19 -0300

    add flexgrid react component and media query

--- functional_programming_reactjs_redux/weather-app/src/App.js
+++ functional_programming_reactjs_redux/weather-app/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import WeatherLocationList from "./components/weatherLocationList";
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 import ExtendedWeather from "./components/ExtendedWeather";
-
+import { Grid, Col, Row } from "react-flexbox-grid";
 const cities = [
   "Buenos Aires, ar",
   "London",
@@ -12,10 +16,9 @@ const cities = [
   "Madrid, es",
 ];
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
-    this.state = {location: 'Seleccione una location'};
+    this.state = { location: "Seleccione una location" };
   }
   updateSelectedLocation = (location) => {
     console.log(location);
@@ -24,17 +27,28 @@ class App extends Component {
 
   render() {
     return (
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: 1 }}>
-          <WeatherLocationList
-            cities={cities}
-            onLocationClicked={this.updateSelectedLocation}
-          ></WeatherLocationList>
-        </div>
-        <div style={{ flex: 1 }}>
-          <ExtendedWeather location={this.state.location}></ExtendedWeather>
-        </div>
-      </div>
+      <Grid>
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}  className='WeatherLocationListCont'>
+            <WeatherLocationList
+              cities={cities}
+              onLocationClicked={this.updateSelectedLocation}
+            ></WeatherLocationList>
+          </Col>
+          <Col xs={12} md={6}>
+            <ExtendedWeather location={this.state.location}></ExtendedWeather>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
--- functional_programming_reactjs_redux/weather-app/src/components/ExtendedWeather/index.js
+++ functional_programming_reactjs_redux/weather-app/src/components/ExtendedWeather/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import './styles.css';
 
-const ExtendedWeather = ({ location }) => <div>{location}</div>;
+const ExtendedWeather = ({ location }) => <div className={'ExtendedWeatherCont'}>{location}</div>;
 
 ExtendedWeather.propTypes = {
   location: PropTypes.string,
