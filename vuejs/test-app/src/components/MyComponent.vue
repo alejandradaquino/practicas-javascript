fb7244c665c4f7a43f8654b0ccd5ce31294df830
alejandra <alejandra.juarez@etermax.com>
2020-07-14 20:30:50 -0300

    vue test app initial commit

---
+++ vuejs/test-app/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+   <h2>{{subtitle}}</h2>
+   <input v-model="message" placeholder="edíteme">
+<p>El mensaje es: {{ message }}</p>
+    </div>
+</template>
+<script>
+export default {
+    
+    name: "my-component",
+    data: ()=>({subtitle:"A subtitle", message: "A Input Messge"})
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
